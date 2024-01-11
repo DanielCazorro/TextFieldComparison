13c52cc628606ba4a01d60bdfa9db455fd8d2638
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-11 16:21:23 +0100

    First Commit: Folders and Layout

---
+++ TextFieldComparison/TextFieldComparison/ViewController/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  TextFieldComparison
+//
+//  Created by Daniel Cazorro Frías on 11/1/24.
+//
+
+import UIKit
+
+
+// MARK: - Class -
+class MainViewController: UIViewController {
+
+    // IBOutlet
+    
+    @IBOutlet weak var textFieldFirst: UITextField!
+    @IBOutlet weak var textFieldSecond: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
---
+++ TextFieldComparison/TextFieldComparison/ViewModel/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  TextFieldComparison
+//
+//  Created by Daniel Cazorro Frías on 11/1/24.
+//
+
+import Foundation
